d2c144330759489dc4dc04ef9e882192ee2660d9
CandyKaine1 <123655765+CandyKaine1@users.noreply.github.com>
2024-01-26 21:01:52 -0500

    Update main.cpp
    changed a few values

--- main.cpp
+++ main.cpp
@@ -1,73 +1,65 @@
 #include <iostream>
 #include <fstream>
-#include <thread>
 #include <vector>
-#include <chrono>
 #include <numeric>
 #include <cmath>
+#include <future>
+#include <chrono>
 
 bool isPrime(int n) {
-  if (n<=1 || n%2==0) return false;
+  if (n <= 1 || n % 2 == 0) return false;
 
-  const int s=sqrt(n);
-  for (int i=3;i<=s;i++){
-    if(n%i==0) return false;
+  for (int i = 3; i * i <= n; i += 2) {
+    if (n % i == 0) return false;
   }
   return true;
 }
 
-
-void compute(int start, int end, int threadId, std::vector<int>& result, int& partialSum) {
+void compute(int start, int end, int threadId, const std::vector<int>& primes, std::vector<int>& result, int& partialSum) {
   for (int i = start; i <= end; ++i) {
     if (isPrime(i)) {
-      result.push_back(i);
-      partialSum +=i;
+      result.emplace_back(i);
+      partialSum += i;
     }
   }
 }
 
 int main() {
-  using namespace std;
-
-  //n is number of Threads, (S,E) is the range.
   const int num = 8;
   const int S = 1;
   const int E = 100000000;
 
-  vector<thread> threads;
-  vector<vector<int>> partial(num);
-  vector<int> sums(num);
-  int totalPrimes=0;
+  std::vector<std::future<void>> futures;
+  std::vector<std::vector<int>> partial(num);
+  std::vector<int> sums(num);
+  int totalPrimes = 0;
 
-  auto start = chrono::high_resolution_clock::now();
+  auto start = std::chrono::high_resolution_clock::now();
 
   for (int i = 0; i < num; ++i) {
     int start = S + (i * (E - S + 1) / num);
     int end = S + ((i + 1) * (E - S + 1) / num) - 1;
-    threads.emplace_back(compute, start, end, i, ref(partial[i]), ref(sums[i]));
+    futures.emplace_back(std::async(std::launch::async, compute, start, end, i, std::cref(partial[i]), std::ref(partial[i]), std::ref(sums[i])));
   }
 
-  // Wait for all threads to finish
-  for (auto& thread : threads) thread.join();
+  // Wait for all futures to finish
+  for (auto& future : futures) future.wait();
 
-  //finish time
-  auto stop = chrono::high_resolution_clock::now();
-  auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+  auto stop = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
 
-  ofstream myfile;
-  myfile.open("primes.txt");
+  std::ofstream myfile("primes.txt");
   myfile << "<" << duration.count() << "ms> ";
 
-  for (const auto& list : partial) totalPrimes+=list.size();
+  for (const auto& list : partial) totalPrimes += list.size();
   myfile << "<" << totalPrimes << "> ";
 
-  int primeSum=accumulate(sums.begin(), sums.end(), 0);
+  int primeSum = std::accumulate(sums.begin(), sums.end(), 0);
   myfile << "<" << primeSum << ">\n<";
 
-  vector<int> destinationVector(partial[num-1].end() - 10, partial[num-1].end());
+  std::vector<int> destinationVector(partial[num - 1].end() - 10, partial[num - 1].end());
   for (int i = 0; i < 10; ++i) myfile << destinationVector[i] << " ";
-  myfile << ">" << endl;
+  myfile << ">" << std::endl;
 
-  myfile.close();
   return 0;
 }
